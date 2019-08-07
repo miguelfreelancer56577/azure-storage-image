b207da6479b6e236d9438f8cfe1cc81048fd93bc
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2019-08-07 17:25:16 -0500

    Added my own starter to work with the azure services and libraries in common.

--- src/main/java/github/com/miguelfreelancer56577/azure_storage_image/config/exception/BlobConfigurationException.java
+++
@@ -1,9 +0,0 @@
-package github.com.miguelfreelancer56577.azure_storage_image.config.exception;
-
-public class BlobConfigurationException extends Exception {
-
-	public BlobConfigurationException(Throwable e){
-		super(e);
-	}
-	
-}
